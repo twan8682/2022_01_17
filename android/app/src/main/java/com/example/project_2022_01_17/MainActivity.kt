99491df0df7217fb14da60b4ebaa2aa147f367e5
twan8682 <twan8682@naver.com>
2022-01-17 14:57:36 +0900

    2022_01_17 안드로이드 프로젝트 생성

---
+++ android/app/src/main/java/com/example/project_2022_01_17/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.project_2022_01_17
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
